70952d034fc25275c1dfae98010a67586d8a0cea
Gert Jan Schoneveld <gj_schoneveld@ymail.com>
2021-12-01 18:21:54 +0100

    Day 1

--- Day01/Program.cs
+++ Day01/Program.cs
@@ -1,5 +1,24 @@
-﻿var input = File.ReadAllLines("input.txt");
+﻿int CountIncrements(List<int> values)
+{
+    return values
+        .Skip(1)
+        .Where((value, index) => value > values[index])
+        .Count();
+}
 
-var answer1 = string.Join(", ", input);
+var input = File.ReadAllLines("input.txt");
+
+var values = input.Select(int.Parse).ToList();
+
+var answer1 = CountIncrements(values);
 
 Console.WriteLine($"Answer 1: {answer1}");
+
+var windowed = values
+    .Skip(2)
+    .Select((value, index) => value + values[index] + values[index + 1])
+    .ToList();
+
+var answer2 = CountIncrements(windowed);
+
+Console.WriteLine($"Answer 2: {answer2}");
