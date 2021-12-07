881bf1d8ebdcc9b991a165ce342209491ea711b7
Gert Jan Schoneveld <gj_schoneveld@ymail.com>
2021-12-07 18:37:54 +0100

    Day 7

--- Day07/Program.cs
+++ Day07/Program.cs
@@ -1,16 +1,16 @@
-﻿int TotalFuelMethod1(Dictionary<int, int> crabs, int position)
+﻿int Fuel1(int distance)
 {
-    return crabs.Sum(kv => Math.Abs(kv.Key - position) * kv.Value);
+    return distance;
 }
 
-int FuelMethod2(int distance)
+int Fuel2(int distance)
 {
     return distance * (distance + 1) / 2;
 }
 
-int TotalFuelMethod2(Dictionary<int, int> crabs, int position)
+int TotalFuel(Func<int, int> fuel, Dictionary<int, int> crabs, int position)
 {
-    return crabs.Sum(kv => FuelMethod2(Math.Abs(kv.Key - position)) * kv.Value);
+    return crabs.Sum(kv => fuel(Math.Abs(kv.Key - position)) * kv.Value);
 }
 
 var input = File.ReadAllText("input.txt");
@@ -24,10 +24,10 @@ var values = input
 var min = values.Min(kv => kv.Key);
 var max = values.Max(kv => kv.Key);
 
-var answer1 = Enumerable.Range(min, max).Min(p => TotalFuelMethod1(values, p));
+var answer1 = Enumerable.Range(min, max).Min(p => TotalFuel(Fuel1, values, p));
 
 Console.WriteLine($"Answer 1: {answer1}");
 
-var answer2 = Enumerable.Range(min, max).Min(p => TotalFuelMethod2(values, p));
+var answer2 = Enumerable.Range(min, max).Min(p => TotalFuel(Fuel2, values, p));
 
 Console.WriteLine($"Answer 2: {answer2}");
